9d4f9ab932edd939c163bb7452f1bd3aa3f0e4b8
EvgrafovBTW <bleedthemwhite@yandex.ru>
2022-02-18 17:15:23 +0700

    created body layout template

---
+++ src/home/body/components/leftPanel.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './leftPanel.css'
+import RightPanel from "./rightPanel";
+
+function LeftPanel() {
+    return(
+        <div className="leftPanel">
+            <div className="resume"></div>
+            <div className="cards"></div>
+            
+        </div>
+    )
+}
+
+export default LeftPanel;
---
+++ src/home/body/components/rightPanel.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './rightPanel.css'
+
+function RightPanel() {
+    return(
+        <div className="rightPanel">
+            <div className="projects"></div>
+        </div>
+    )
+}
+
+export default RightPanel;
